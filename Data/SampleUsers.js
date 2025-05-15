d0e72e4099f9298e9c0fee2dd4ac1ac98ee991aa
Deepak garg <deepak1681.be21@chitkara.edu.in>
2025-05-15 16:46:02 +0530

    Add files via upload

---
+++ Data/SampleUsers.js
@@ -0,0 +1,62 @@
+export const sampleUsers = [
+  {
+    firstname: "Deepak",
+    lastname: "Garg",
+    username: "deepakgarg",
+    password: "Deepak123",
+    isAdmin: false,
+    profilePicture: "n/a",
+    coverPicture: "n/a",
+    about: "n/a",
+    livesIn: "n/a",
+    worksAt: "n/a",
+    relationship: "n/a",
+    followers: [],
+    following: []
+  },
+  {
+    firstname: "Rhy",
+    lastname: "Garg",
+    username: "rhygarg",
+    password: "Rhy123",
+    isAdmin: false,
+    profilePicture: "n/a",
+    coverPicture: "n/a",
+    about: "n/a",
+    livesIn: "n/a",
+    worksAt: "n/a",
+    relationship: "n/a",
+    followers: [],
+    following: []
+  },
+  {
+    firstname: "Ankush",
+    lastname: "Garg",
+    username: "ankushgarg",
+    password: "Ankush123",
+    isAdmin: false,
+    profilePicture: "n/a",
+    coverPicture: "n/a",
+    about: "n/a",
+    livesIn: "n/a",
+    worksAt: "n/a",
+    relationship: "n/a",
+    followers: [],
+    following: []
+  },
+  {
+    firstname: "Raj",
+    lastname: "Garg",
+    username: "rajgarg",
+    password: "Raj123",
+    isAdmin: false,
+    profilePicture: "n/a",
+    coverPicture: "n/a",
+    about: "n/a",
+    livesIn: "n/a",
+    worksAt: "n/a",
+    relationship: "n/a",
+    followers: [],
+    following: []
+  }
+]; 
